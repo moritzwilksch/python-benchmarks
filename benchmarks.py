3dd56590c5e322b4ad98865ac0c868fffc28bb90
Moritz Wilksch <moritzwilksch@gmail.com>
2022-02-16 15:02:23 +0100

    add scatterplot test

--- benchmarks.py
+++ benchmarks.py
@@ -5,11 +5,13 @@ import numpy as np
 from typing import List
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
 import nltk
 import re
 import seaborn as sns
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-nltk.download('gutenberg')
+
+nltk.download("gutenberg")
 c = Console(record=True)
 
 # UTILITIES
@@ -22,18 +24,21 @@ def timer(f: callable, *args, **kwargs):
         tac = time.time()
         c.print(f"-> took {tac - tic:8.5f} seconds.")
         return result
+
     return wrapper
 
 
-def benchmark_runner(bench_function: callable, title: str = "", n_runs: int = 3, *args, **kwargs):
+def benchmark_runner(
+    bench_function: callable, title: str = "", n_runs: int = 3, *args, **kwargs
+):
     c.print(f"{title:=^40}")
     tic = time.time()
     for run in range(n_runs):
         bench_function(*args, **kwargs)
     tac = time.time()
-    c.print("-"*40)
+    c.print("-" * 40)
     c.print(f"AVG = {(tac - tic) / n_runs:.5f} seconds.")
-    c.print("-"*40)
+    c.print("-" * 40)
 
 
 # BENCHMARK FUNCTIONS
@@ -53,9 +58,9 @@ def monte_carlo():
 def string_processing(word_list: List[str]):
     s = " ".join(word_list)
     l = (x.strip().lower() for x in s.split())
-    l = (x for x in l if x != '')
-    l = (x.upper() if x[0] == 'a' else x.lower() for x in l)
-    is_year = [re.match(r'\d{4}', x) for x in l]
+    l = (x for x in l if x != "")
+    l = (x.upper() if x[0] == "a" else x.lower() for x in l)
+    is_year = [re.match(r"\d{4}", x) for x in l]
 
     return " ".join(l), is_year
 
@@ -82,6 +87,13 @@ def fit_rf_synthetic(X, y):
     return "Done."
 
 
+@timer
+def scatterplotting(df: pd.DataFrame):
+    sns.scatterplot(data=df, x="a", y="b")
+    plt.savefig("test.png")
+    return "Done."
+
+
 # EXECUTION
 # Matrix Multiply
 a = np.random.randint(-100, 100, (3_000, 100)).astype(np.int16)
@@ -95,21 +107,41 @@ del a, b
 benchmark_runner(monte_carlo, n_runs=5, title="Monte Carlo Random Walk")
 
 # String Processing
-emma = nltk.corpus.gutenberg.words('austen-emma.txt')
+emma = nltk.corpus.gutenberg.words("austen-emma.txt")
 benchmark_runner(string_processing, n_runs=5, title="String Processing", word_list=emma)
 
 benchmark_runner(tfidf_convert, n_runs=5, title="TFIDF from string", word_list=emma)
 del emma
 
 # Fitting RF with all cores
-data = sns.load_dataset('tips')
-catcols = data.select_dtypes('category').columns
+data = sns.load_dataset("tips")
+catcols = data.select_dtypes("category").columns
 for col in catcols:
     data[col] = data[col].cat.codes
 
-benchmark_runner(fit_rf, n_runs=5, X=data.drop('tip', axis=1), y=data.tip, title="Fitting RF in parallel")
+benchmark_runner(
+    fit_rf,
+    n_runs=5,
+    X=data.drop("tip", axis=1),
+    y=data.tip,
+    title="Fitting RF in parallel",
+)
 
-benchmark_runner(fit_rf, n_runs=1, X=np.random.random((5_000, 15)), y=np.random.random(5_000), title="Fitting RF in parallel on _synthetic_ data")
+benchmark_runner(
+    fit_rf,
+    n_runs=1,
+    X=np.random.random((5_000, 15)),
+    y=np.random.random(5_000),
+    title="Fitting RF in parallel on _synthetic_ data",
+)
+
+
+benchmark_runner(
+    scatterplotting,
+    n_runs=1,
+    df=pd.DataFrame(np.random.random((1_000_000, 2)), columns=["a", "b"]),
+    title="Scatterplotting 1M points",
+)
 
 # SAVE LOG
 c.save_text("benchmarks.log")
